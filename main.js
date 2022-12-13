a76945ba85130ea11efecd627b8a081e7952a0c1
Vivek <vpkedare.97@gmail.com>
2022-12-13 15:43:28 +0530

    history sidebar added

--- main.js
+++ main.js
@@ -9,18 +9,22 @@ const displaySolution = document.getElementById("displaySolution");
 const displayEquation = document.getElementById("displayEquation");
 const displayHistory = document.querySelector(".display-history");
 const history = document.querySelector(".history");
+const closeBtn = document.querySelector(".close-btn");
+
 let equations;
 let equationsArray = [];
 let answersArray = [];
 
 // Display input on screen
 function displayInput(inputValue) {
-   (displaySolution.value === "0") ? displaySolution.value = inputValue : displaySolution.value += inputValue;   
+   displaySolution.value === "0"
+      ? (displaySolution.value = inputValue)
+      : (displaySolution.value += inputValue);
 }
 
 // To display numbers
 numberBtns.forEach(numberBtn => {
-   numberBtn.addEventListener('click', (e) => {
+   numberBtn.addEventListener("click", e => {
       const number = e.target.innerText;
       displayInput(number);
    });
@@ -28,38 +32,42 @@ numberBtns.forEach(numberBtn => {
 
 // To display operations
 operationBtns.forEach(operationBtn => {
-   operationBtn.addEventListener('click', (e) => {
+   operationBtn.addEventListener("click", e => {
       const operation = e.target.innerText;
       displayInput(operation);
    });
 });
 
 // Calculate values
-equalToBtn.addEventListener('click', () => {
+equalToBtn.addEventListener("click", () => {
    equations = displaySolution.value;
    equations = `${equations} = `;
-   
+
    displayEquation.value = equations;
    displaySolution.value = eval(displaySolution.value);
    addToHistory();
+   checkNumberIsFloat(eval(displaySolution.value));
 });
 
 // Clear Display
-clearInputBtn.addEventListener('click', () => {
+clearInputBtn.addEventListener("click", () => {
    displaySolution.value = 0;
    displayEquation.value = "";
 });
 
 // Hide and show history
-historyBtn.addEventListener('click', () => {
+historyBtn.addEventListener("click", () => {
    history.classList.toggle("show-history");
 });
 
+// Closing with cross button
+closeBtn.addEventListener('click', () => history.classList.toggle("show-history"));
+
 // Clear history
-clearHistoryBtn.addEventListener('click', () => {
+clearHistoryBtn.addEventListener("click", () => {
    const confirmAction = confirm("Are you sure you want to clear the history?");
-   
-   if (confirmAction){
+
+   if (confirmAction) {
       displayHistory.innerText = "";
       equationsArray = [];
       answersArray = [];
@@ -67,31 +75,31 @@ clearHistoryBtn.addEventListener('click', () => {
 });
 
 // Backspace functionality
-deleteStringBtn.addEventListener('click', () => {
+deleteStringBtn.addEventListener("click", () => {
    displayEquation.value = "";
-   
-   if(displaySolution.value !== "") {
-      if(displaySolution.value.length > 1) {
-         displaySolution.value = displaySolution.value.slice(0,-1);
-      }
-      else {
+
+   if (displaySolution.value !== "") {
+      if (displaySolution.value.length > 1) {
+         displaySolution.value = displaySolution.value.slice(0, -1);
+      } else {
          displaySolution.value = "0";
       }
    }
 });
 
+
 // Store it to history
 function addToHistory() {
    equationsArray.push(equations);
-   answersArray.push(eval(displaySolution.value));
+   answersArray.push(displaySolution.value);
    let historyDiv = "";
    equationsArray.reverse();
    answersArray.reverse();
-   for(let i = 0; i < answersArray.length; i++) {
+   for (let i = 0; i < answersArray.length; i++) {
       historyDiv += `
       <div class='history-div'>
          <p id="${i}">
-            <span>${i+1}]</span> 
+            <span>${i + 1}]</span> 
             ${equationsArray[i]} ${answersArray[i]}
             </p>
       </div>`;
@@ -100,8 +108,17 @@ function addToHistory() {
 }
 
 // Copy from history to display
-displayHistory.addEventListener('click', (e) => {
+displayHistory.addEventListener("click", e => {
    const index = e.target.id;
    displaySolution.value = answersArray[index];
    displayEquation.value = equationsArray[index];
 });
+
+function checkNumberIsFloat(resultValue) {
+   if(Number.isInteger(resultValue)) {
+      return;
+   }
+   else {
+      displaySolution.value = resultValue.toFixed(2);
+   }
+}
